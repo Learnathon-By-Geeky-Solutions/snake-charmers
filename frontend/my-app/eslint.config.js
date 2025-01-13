c65ce84ae86faf971433fa5174996bb918b6277a
Imdad Rakib <rakibattaridib@gmail.com>
2025-01-13 11:27:35 +0600

    Initiate vite react app with tailwind configuration

---
+++ frontend/my-app/eslint.config.js
@@ -0,0 +1,47 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+
+      'no-console': 'warn', // Warn on console statements
+      'quotes': ['error', 'single'], // Enforce single quotes
+      'semi': ['error', 'always'], // Enforce semicolons
+      'indent': ['error', 2], // Enforce 2-space indentation
+      'no-unused-vars': ['warn', { 'args': 'none' }], // Warn on unused variables, ignoring unused function arguments
+      'eqeqeq': ['error', 'always'], // Enforce strict equality (=== and !==)
+      // 'no-magic-numbers': ['warn', { 'ignore': [0, 1] }], // Warn on magic numbers except 0 and 1
+      // 'max-len': ['warn', { 'code': 150 }] // Warn if a line exceeds 80 characters
+    },
+  },
+];
