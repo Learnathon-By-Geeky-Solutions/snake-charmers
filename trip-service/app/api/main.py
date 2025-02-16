715b9c3c1b70490804fb119e429ed1864593f02a
Imdad Rakib <rakibattaridib@gmail.com>
2025-02-16 14:28:28 +0600

    refactor(trip-service): Resolve pylint errors
    Added better exception handing using exc, shortened the long lines for
    better readibility, remove errror duplication using global constant.

--- trip-service/app/api/main.py
+++ trip-service/app/api/main.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, select
-from app.schemas.main import TripRequestCreate, TripRequestResponse, TripCreate,TripResponse, EngageDriverRequest, UpdateTripStatusRequest
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from app.schemas.main import (
+   TripRequestCreate,
+   TripRequestResponse,
+   TripCreate,
+   TripResponse,
+   EngageDriverRequest,
+   UpdateTripStatusRequest
+)
 from app.services.main import TripService
 from app.db import get_session
 
@@ -8,7 +15,10 @@ router = APIRouter(prefix="/api/trip", tags=["Trip Service"])
 
 
 @router.post("/request/add", response_model=TripRequestResponse, status_code=201)
-def add_trip_request(request_data: TripRequestCreate, db: Session = Depends(get_session)):
+def add_trip_request(
+    request_data: TripRequestCreate,
+    db: Session = Depends(get_session)
+):
     return TripService.add_trip_request(db, request_data)
 
 
@@ -30,10 +40,12 @@ def release_driver(driver_id : int, db: Session = Depends(get_session)):
     return TripService.release_driver(db, driver_id)
 
 @router.post("/add", response_model=TripResponse, status_code=201)
-def add_trip(trip_data: TripCreate, db: Session = Depends(get_session)): 
+def add_trip(trip_data: TripCreate, db: Session = Depends(get_session)):
     return TripService.add_trip(db, trip_data)
 
-
 @router.put("/status", status_code=200)
-def update_trip_status(request: UpdateTripStatusRequest, db: Session = Depends(get_session)):
+def update_trip_status(
+    request: UpdateTripStatusRequest,
+    db: Session = Depends(get_session)
+):
     return TripService.update_trip_status(db, request.trip_id, request.status)
