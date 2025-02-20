4431affd830dae56485c8107f378f6b839444526
Imdad Rakib <rakibattaridib@gmail.com>
2025-02-20 22:07:54 +0600

    fix(Dockerfile): Address security issues in Dockerfile.
    Dockerfile updated to run the containers as a non root user.

--- trip-service/app/services/main.py
+++ trip-service/app/services/main.py
@@ -20,26 +20,24 @@ class TripService:
 
     @staticmethod
     def remove_trip_request(db: Session, req_id: int):
+
+        trip_request = db.get(TripRequest, req_id)
+        if not trip_request:
+            raise HTTPException(status_code=404, detail="Trip request not found")
         try:
-            trip_request = db.get(TripRequest, req_id)
-            if not trip_request:
-                raise HTTPException(status_code=404, detail="Trip request not found")
             db.delete(trip_request)
             db.commit()
             return {"success": True}
-
         except Exception as exc:
             db.rollback()
             raise INTERNAL_SERVER_ERROR from exc
 
     @staticmethod
     def engage_driver(db: Session, req_id: int, driver_id: int):
+        trip = db.get(TripRequest, req_id)
+        if not trip:
+            raise HTTPException(status_code=404, detail="Trip request not found")
         try:
-            trip = db.get(TripRequest, req_id)
-
-            if not trip:
-                raise HTTPException(status_code=404, detail="Trip request not found")
-
             engagement = EngagedDriver(req_id=req_id, driver_id=driver_id)
             db.add(engagement)
             db.commit()
@@ -51,13 +49,14 @@ class TripService:
 
     @staticmethod
     def release_driver(db: Session, driver_id: int):
-        try:
-            engaged_driver = db.exec(
-            select(EngagedDriver).where(EngagedDriver.driver_id == driver_id)
-            ).first()
-            if not engaged_driver:
-                raise HTTPException(status_code=404, detail="Driver not found")
 
+        engaged_driver = db.exec(
+            select(EngagedDriver).where(EngagedDriver.driver_id == driver_id),
+        ).first()
+
+        if not engaged_driver:
+            raise HTTPException(status_code=404, detail="Driver not found")
+        try:
             db.delete(engaged_driver)
             db.commit()
             return {"success": True}
@@ -79,14 +78,13 @@ class TripService:
 
     @staticmethod
     def update_trip_status(db: Session, trip_id: int, status: str):
+        trip = db.get(Trip, trip_id)
+        if not trip:
+            raise HTTPException(status_code=404, detail="Trip not found")
         try:
-            trip = db.get(Trip, trip_id)
-            if not trip:
-                raise HTTPException(status_code=404, detail="Trip not found")
             trip.status = status
             db.commit()
             return {"success": True}
-
         except Exception as exc:
             db.rollback()
             raise INTERNAL_SERVER_ERROR from exc
