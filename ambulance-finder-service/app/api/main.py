5922d0517f4d99945b8f5c01757e2a40d53a7852
SA-K1B <tareqshakib789@gmail.com>
2025-02-28 19:21:23 +0600

    feat(ambulance-finder-service): Complete ambulance finder service

--- ambulance-finder-service/app/api/main.py
+++ ambulance-finder-service/app/api/main.py
@@ -7,10 +7,10 @@ from app.db import get_session
 router = APIRouter(prefix="/api/ambulance", tags=["Ambulance Finder Service"])
 
 # for testing purposes
-@router.post("/", response_model=DriverLocationResponse,status_code=201)  
-def update_driver_location(location_data: DriverLocationCreate, db: Session = Depends(get_session)):
-    # print("test in update_driver_location api")
-    return AmbulanceService.update_driver_location(db, location_data)
+# @router.post("/", response_model=DriverLocationResponse,status_code=201)  
+# def update_driver_location(location_data: DriverLocationCreate, db: Session = Depends(get_session)):
+#     # print("test in update_driver_location api")
+#     return AmbulanceService.update_driver_location(db, location_data)
 
 @router.get("/nearby", response_model=list[DriverLocationResponse],status_code=200)
 def find_nearby_drivers(request: NearbyDriversRequest = Depends(), db: Session = Depends(get_session)):
