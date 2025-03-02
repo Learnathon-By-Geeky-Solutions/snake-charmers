c5760f7a704ad6053c25da57768b1474af3a8913
SA-K1B <tareqshakib789@gmail.com>
2025-03-02 19:12:22 +0600

    feat(location-service): Implement api endpoint for updating driver location

--- location-service/app/services/main.py
+++ location-service/app/services/main.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, select
 from fastapi import HTTPException
 from app.models.main import DriverLocation
-from geoalchemy2.functions import ST_Distance, ST_GeomFromText
+from geoalchemy2.functions import  ST_GeomFromText
 resolution = 10
 
 def add_driver_location(
@@ -26,3 +26,24 @@ def add_driver_location(
     except Exception as exc:
              session.rollback()
              raise HTTPException(status_code=500, detail="Internal server error") from exc
+def update_driver_location(
+        session: Session,
+        driver_id: int,
+        latitude: float,
+        longitude: float
+) -> None:
+    """
+    Update the latitude and longitude of a driver's location in the database.
+    """
+    try:
+       point = f'POINT({longitude} {latitude})'
+       #Check if driver location exists
+       driver_location = session.query(DriverLocation).filter(
+       DriverLocation.driver_id == driver_id
+       ).first()  
+       driver_location.location = ST_GeomFromText(point, 4326)
+       session.merge(driver_location)
+    except Exception as exc:
+       session.rollback()
+       raise HTTPException(status_code=500, detail="Internal server error") from exc
+
