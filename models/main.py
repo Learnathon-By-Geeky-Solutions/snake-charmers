4c9d08ed11e3e9f4cc78154f0c04b5cff854c70e
SA-K1B <tareqshakib789@gmail.com>
2025-03-05 23:21:49 +0600

    Fix(models): Update fields for handling geospatial data

--- models/main.py
+++ models/main.py
@@ -4,8 +4,10 @@ from geoalchemy2 import Geography
 
 DRIVER_ID_FK = "driver.driver_id"
 
+
 class Driver(SQLModel, table=True):
-    driver_id: Optional[int] = Field(default=None, primary_key=True, index=True)
+    driver_id: Optional[int] = Field(
+        default=None, primary_key=True, index=True)
     name: str
     mobile: str = Field(unique=True, nullable=False)
     email: str = Field(unique=True, nullable=False)  # Mandatory and unique
@@ -29,11 +31,13 @@ class DriverLocation(SQLModel, table=True):
             primary_key=True,
             index=True
         )
-    ) 
-    location: Geography = Field(sa_column=Column(Geography(geometry_type="POINT", srid=4326), nullable=False))
+    )
+    location: Geography = Field(sa_column=Column(
+        Geography(geometry_type="POINT", srid=4326), nullable=False))
     model_config = {
         "arbitrary_types_allowed": True
     }
+
 
 class Trip(SQLModel, table=True):
     trip_id: Optional[int] = Field(default=None, primary_key=True, index=True)
@@ -51,8 +55,10 @@ class Trip(SQLModel, table=True):
             index=True
         )
     )
-    pickup_location: str
-    destination: str
+    pickup_location: Geography = Field(sa_column=Column(
+        Geography(geometry_type="POINT", srid=4326), nullable=False))
+    destination: Geography = Field(sa_column=Column(
+        Geography(geometry_type="POINT", srid=4326), nullable=False))
     fare: float
     status: str  # e.g., "completed", "cancelled", etc.
 
@@ -61,27 +67,29 @@ class TripRequest(SQLModel, table=True):
     req_id: Optional[int] = Field(default=None, primary_key=True, index=True)
     rider_id: int = Field(
         sa_column=Column(
-            Integer, 
+            Integer,
             ForeignKey("rider.rider_id", ondelete="CASCADE"),
             index=True
         )
     )
-    pickup_location: str
-    destination: str
+    pickup_location: Geography = Field(sa_column=Column(
+        Geography(geometry_type="POINT", srid=4326), nullable=False))
+    destination: Geography = Field(sa_column=Column(
+        Geography(geometry_type="POINT", srid=4326), nullable=False))
 
 
 class EngagedDriver(SQLModel, table=True):
     req_id: int = Field(
         sa_column=Column(
-            Integer, 
-            ForeignKey("triprequest.req_id", ondelete="CASCADE"),  
+            Integer,
+            ForeignKey("triprequest.req_id", ondelete="CASCADE"),
             primary_key=True,
             index=True
         )
     )
     driver_id: int = Field(
         sa_column=Column(
-            Integer, 
+            Integer,
             ForeignKey(DRIVER_ID_FK, ondelete="CASCADE"),
             index=True
         )
