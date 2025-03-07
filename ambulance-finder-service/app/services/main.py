4f8e1d27c316c46fa270b81ef5beb2e99b85c609
SA-K1B <tareqshakib789@gmail.com>
2025-03-07 15:09:03 +0600

    Fix(ambulance-finder-service): Update implementation for ambulance finder

--- ambulance-finder-service/app/services/main.py
+++ ambulance-finder-service/app/services/main.py
@@ -1,8 +1,10 @@
 from sqlmodel import Session
 from geoalchemy2.functions import ST_Distance, ST_GeomFromText
 from app.schemas.main import NearbyDriversRequest
-from app.models.main import DriverLocation, Driver
+from app.models.main import DriverLocation, Driver, EngagedDriver
 from fastapi import HTTPException
+from sqlalchemy.orm import aliased
+
 
 class AmbulanceService:
     @staticmethod
@@ -10,7 +12,12 @@ class AmbulanceService:
         try:
             # Create reference point
             ref_point = f'POINT({request.lon} {request.lat})'
-            # Query nearby drivers with a join to the Driver table
+
+            # Subquery to get engaged drivers
+            engaged_drivers_subquery = db.query(
+                EngagedDriver.driver_id).subquery()
+
+            # Query nearby drivers with a join to the Driver table and exclude engaged drivers
             results = db.query(
                 DriverLocation.driver_id,
                 Driver.name,
@@ -22,7 +29,10 @@ class AmbulanceService:
                     DriverLocation.location,
                     ST_GeomFromText(ref_point, 4326)
                 ) <= request.radius * 1000  # Convert km to meters
+            ).filter(
+                DriverLocation.driver_id.notin_(engaged_drivers_subquery)
             ).all()
+
             # Format the results
             return [
                 {
