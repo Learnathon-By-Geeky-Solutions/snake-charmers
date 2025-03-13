3b27d5ce724693b164e0cac0a8fce7e2d0aa6787
SA-K1B <tareqshakib789@gmail.com>
2025-03-13 20:18:09 +0600

    test(location-service): Change unit test logic for adding location

--- location-service/tests/unittest/test_add_location.py
+++ location-service/tests/unittest/test_add_location.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi import status
 from app.schemas.main import AddDriverLocationRequest, LocationAddResponse
+from app.schemas.main import UpdateDriverLocationRequest, LocationUpdateResponse, LocationRemoveResponse
 
 
 def test_add_driver_location(client, session):
@@ -14,12 +15,9 @@ def test_add_driver_location(client, session):
     )
 
     # Send the POST request to add the driver's location
+
     response = client.post("/api/location/add", json=request_payload.dict())
 
     # Assert the response status code and content
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-        "driver_id": 1,
-        "latitude": 37.7749,
-        "longitude": -122.4194
-    }
+    assert response.json() == {"success": True, "message": None}
